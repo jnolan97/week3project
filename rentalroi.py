fc02d73a528ea5393cc0aa50b33b4bbf11aff2e0
jnolan97 <johnmnolan97@gmail.com>
2020-05-22 12:35:07 -0500

    First commit, included cashFlow and cashROI methods

---
+++ rentalroi.py
@@ -0,0 +1,29 @@
+class Rental():
+    def __init__(self,income,expenses):
+        self.income = income
+        self.expenses = expenses
+    
+    #calculate cash flow
+    def cashFlow(self):
+        return print(self.income - self.expenses)
+    
+    def cashROI(self):
+        down_payment = input("What did you put down on this house? ")
+        costs = input("What were your closing costs? ")
+        rehab = input("What did you spend on fixes? ")
+        total_cost = float(down_payment + costs + rehab)
+        annual = float(12 * (self.income - self.expenses))
+        invest = float((annual / total_cost) * 100)
+        print(invest,"%")
+
+def run():
+    buyer = Rental(2000,1610)
+    while True:
+        response = input("What do you want to do today? ")
+        if response.lower() == "cashflow":
+            buyer.cashFlow()
+        if response.lower() == "roi":
+            buyer.cashROI()
+        if response.lower() == "exit":
+            break
+run()
